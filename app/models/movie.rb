d8b9a56548424a60885ffecf7f2f7de5bc53568e
Amanda Horowitz <amanda.horowitz@kellogg.northwestern.edu>
2023-05-05 02:16:31 +0000

    movie1 added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-    puts "hello"
+    movie1 = Movie.new
+    movie1["title"] = "Batman Begins"
+    movie1["year_released"] = "2005"
+    movie1["rated"] = "PG-13"
+    movie1.save
 end
