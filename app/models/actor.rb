439e23c7efddb17311f12082835628f6db478cad
Amanda Horowitz <amanda.horowitz@kellogg.northwestern.edu>
2023-05-05 02:41:11 +0000

    actor5 commit

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ApplicationRecord
+    actor1 = Actor.new
+    actor1["name"] = "Christian Bale"
+    puts actor1.inspect
+
+    actor2 = Actor.new
+    actor2["name"] = "Michael Cane"
+    puts actor2.inspect
     
+    actor3 = Actor.new
+    actor3["name"] = "Liam Neeson"
+    puts actor3.inspect
+
+    actor4 = Actor.new
+    actor4["name"] = "Katie Holmes"
+    puts actor4.inspect
+
+    actor5 = Actor.new
+    actor5["name"] = "Gary Oldman"
+    puts actor5.inspect
+
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,7 @@ class Movie < ApplicationRecord
     movie1.save
     movie1 = Movie.where({"title" => "Batman Begins", "year_released" => "2005", "rated" => "PG-13"})[0]
     puts movie1.inspect
+    
     movie2 = Movie.new
     movie2["title"] = "The Dark Knight"
     movie2["year_released"] = "2008"
@@ -13,6 +14,7 @@ class Movie < ApplicationRecord
     movie2.save
     movie2 = Movie.where({"title" => "The Dark Knight", "year_released" => "2008", "rated" => "PG-13"})[0]
     puts movie2.inspect
+    
     movie3 = Movie.new
     movie3["title"] = "The Dark Knight Rises"
     movie3["year_released"] = "2012"
