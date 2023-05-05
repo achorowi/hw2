9e54c3f3ee40614c3bb16c058ba5eba4a78674ce
Amanda Horowitz <amanda.horowitz@kellogg.northwestern.edu>
2023-05-05 02:23:20 +0000

    movie1 edit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
     movie1["year_released"] = "2005"
     movie1["rated"] = "PG-13"
     movie1.save
+    movie1 = Movie.where({"title" => "Batman Begins", "year_released" => "2005", "rated" => "PG-13"})[0]
+    puts movie1.inspect
 end
